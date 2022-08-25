9fde4f0b91446aa77eeb80b46b2c06ca24cb97d2
ihch <nemu0sou@gmail.com>
2022-08-25 15:29:25 +0900

    feat: fetch process

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import React from 'react';
+import { Fetcher } from 'openapi-typescript-fetch';
+import useSWR from 'swr';
+
 import './App.css';
+import { paths } from './openapi/openapi';
+
+const fetcher = Fetcher.for<paths>();
+
+fetcher.configure({
+  baseUrl: 'http://localhost:8000',
+});
+
+const fetchRoot = fetcher.path('/').method('get').create();
 
 function App() {
+  const { data, error } = useSWR('/', fetchRoot);
+
+  if (error) {
+    return <div>error</div>
+  }
+
+  if (!data) {
+    return <div>loading</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <p>{JSON.stringify(data.data)}</p>
     </div>
   );
 }
